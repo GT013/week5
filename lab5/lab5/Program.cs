d982ebeb2c8ce01e03c2f3bc836c5a29bb2bdcd3
nantikan <60030049@kmitl.ac.th>
2018-09-14 13:48:33 +0700

    add1

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line 1");
+            goto line4;          
+
+            line2:
+            Console.WriteLine("Line 2");
+            Console.WriteLine("Line 3");
+            line4:
+            Console.WriteLine("Line 4");
+            Console.WriteLine("Line 5");
+            
+            Console.WriteLine("Line 6");           
+            Console.WriteLine("Line 7");
+           
+            Console.WriteLine("Line 8");
+            line9:
+            Console.WriteLine("Line 9");
+                      
+            Console.WriteLine("Line 10");
+            
+            
+
+        }
+    }
+}
