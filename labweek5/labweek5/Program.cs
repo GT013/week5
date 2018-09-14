2d74e692eef7a760dc20191cd650abf14ac45c9f
nantikan <60030049@kmitl.ac.th>
2018-09-14 14:18:52 +0700

    data2

--- labweek5/labweek5/Program.cs
+++ labweek5/labweek5/Program.cs
@@ -10,7 +10,36 @@ namespace labweek5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello week5");
+            Console.WriteLine("Coffee sizes: \n 1 = Small\n 2 = Medium\n 3 = Large");
+            Console.Write("Please enter your selection: ");
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            int cost = 0;
+            switch (n)
+            {
+                case 1:
+                    cost += 25;
+                    break;
+                case 2:
+                    cost += 25;
+                    goto case 1;
+                case 3:
+                    cost += 50;
+                    goto case 1;
+                default:
+                    Console.WriteLine("Invalid selection.");
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Please insert {0} Bath.", cost);
+            }
+            Console.WriteLine("Thank you for your business.");
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }
