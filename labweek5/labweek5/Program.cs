ca0b2e75c5cd380561ab1eafeae2a69688d9b407
nantikan <60030049@kmitl.ac.th>
2018-09-14 15:17:23 +0700

    data6

--- labweek5/labweek5/Program.cs
+++ labweek5/labweek5/Program.cs
@@ -10,7 +10,10 @@ namespace labweek5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello week5");
+            int a = int.MaxValue;
+            a *= 2;
+            Console.WriteLine(a);
+
         }
     }
 }
