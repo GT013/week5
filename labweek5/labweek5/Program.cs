2bc20d575542e13d0abee5e508a0d8a231a9cdd3
nantikan <60030049@kmitl.ac.th>
2018-10-05 14:45:50 +0700

    add

--- labweek5/labweek5/Program.cs
+++ labweek5/labweek5/Program.cs
@@ -10,7 +10,15 @@ namespace labweek5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello week5");
+            long c = Factorial(5); // long เหมือนกับ int แต่จะมีขนาดมากกว่า ใส่ค่าอาร์กิวเมนต์ 5
+            Console.WriteLine("Factorial of 5 = {0}", c); // แสดงข้อความและผลลัพธืที่ได้จากการคำนวณ
+            Console.ReadLine();
+        }
+        static long Factorial(int n) // สร้างเมธอด 
+        {
+            if (n == 0)  // Factorial 0 = 1
+                return 1;
+            return n * Factorial(n - 1); // 
         }
     }
 }
