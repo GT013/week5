6784d39bc2ee61efee8764a57c7f2148c6af3963
nantikan <60030049@kmitl.ac.th>
2018-10-05 14:38:17 +0700

    add

--- labweek5/labweek5/Program.cs
+++ labweek5/labweek5/Program.cs
@@ -10,14 +10,21 @@ namespace labweek5
     {
         static void Main(string[] args)
         {
-                Number("60030049");
-                Console.ReadLine();
-
-        void Number(string num)
-            {
-                Console.WriteLine(num);
-            }
-
+            int a = 1;
+            int b = 2;
+            int c;
+            c = add(a, b); // ให้ c เก็บค่า a+b จากเมธอด add
+            Console.WriteLine("c = {0}", c); // แสดงค่า c คือ ค่าของ a+b 
+            Console.WriteLine("3 + 5 = {0}", add(3, 5)); // เรียกใช้เมธอด add(a+b) 
+            Console.WriteLine("4 + 7 =  " + add(4, 7)); // เรียกใช้เมธอด add(a+b) เครื่องหมายบวกคือให้แสดงผลลัพธ์ต่อจาก =
+            Console.ReadLine();
         }
+
+        static int add(int a, int b) // เมธอดที่สร้างขึ้นมีชนิดเป็นจำนวนเต็ม
+        {
+            return a + b; // ค่าส่งกลับ
+        }
+
     }
 }
+
